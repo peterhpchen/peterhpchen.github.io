09603bbf994c4bceb007952421194d8ee7f7eff4
peterhpchen <peterhsinpingchen@gmail.com>
2023-06-09 11:00:54 +0800

    init porfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://peterhpchen.github.io',
-  base: '/',
-  integrations: [tailwind()]
-});
+export default defineConfig({});
